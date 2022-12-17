a372fcfb60fb57d99563cb68d4401bfb4f36b47a
Olena-Vozniuk <110108199+Olena-Vozniuk@users.noreply.github.com>
2022-12-17 17:43:20 +0100

    ready video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+player.setCurrentTime(JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)))
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
+    }
+  });
+
+player.on(
+  'timeupdate',
+  throttle(({ seconds }) => {
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(seconds));
+  }, 1000)
+);
